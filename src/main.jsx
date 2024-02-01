817b79bb3730c4358b0facc2d38f5af79f8ec4e8
유애나 <77397206+Teerapat28@users.noreply.github.com>
2024-02-01 17:14:50 +0700

    userSlice / register, login Placehoders

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "normalize.css"
 import './index.css'
+import { store } from './store.js'
+import { Provider } from 'react-redux'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  
 )
