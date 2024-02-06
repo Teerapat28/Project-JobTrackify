268e8fcec3b2999796f9ee2d362e05193354172a
유애나 <77397206+Teerapat28@users.noreply.github.com>
2024-02-06 19:46:31 +0700

    Smallsidebar / navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           >
             <Route index element={<Stats />} />
             <Route path="all-jobs" element={<AllJobs />} />
-            <Route path="add-jobs" element={<AddJobs />} />
+            <Route path="add-job" element={<AddJobs />} />
             <Route path="profile" element={<Profile />} />
           </Route>
           <Route path="/landing" element={<Landing />} />
--- src/assets/components/SmallSidebar.jsx
+++ src/assets/components/SmallSidebar.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import Wrapper from "../wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../../features/user/userSlice";
+import NavLinks from "./NavLinks";
+
 
 const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
   return (
     <>
       <Wrapper>
-        <h2>SmallSidebar</h2>
+        <div
+          className={
+            isSidebarOpen
+              ? "sidebar-container show-sidebar"
+              : "sidebar-container"
+          }
+        >
+          <div className="content">
+            <button className="close-btn" onClick={toggle}>
+              <FaTimes />
+            </button>
+            <header>
+              <Logo />
+            </header>
+            <NavLinks toggleSidebar={toggle} />
+          </div>
+        </div>
       </Wrapper>
     </>
   );
