ab28a6d7c1e2881e87d9d2428be5e1d6430015d7
유애나 <77397206+Teerapat28@users.noreply.github.com>
2024-02-07 21:18:24 +0700

    bigSidebar / Profile page

--- src/assets/components/BigSidebar.jsx
+++ src/assets/components/BigSidebar.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Wrapper from "../wrappers/BigSidebar";
+import NavLinks from "./NavLinks";
+import { Logo } from "../components";
+import { useSelector } from "react-redux";
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <>
       <Wrapper>
-        <h2>BigSidebar</h2>
+        <div
+          className={
+            isSidebarOpen
+              ? "sidebar-container"
+              : "sidebar-container show-sidebar"
+          }
+        >
+          <div className="content">
+            <header>
+              <Logo />
+            </header>
+            <NavLinks />
+          </div>
+        </div>
       </Wrapper>
     </>
   );
